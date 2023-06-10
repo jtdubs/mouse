caddd0896abbf3a493cc12a1e17fd49ebbeb602d
Justin Dubs <jtdubs@gmail.com>
2023-06-10 18:18:37 -0400

    rename cmouse to mouse

--- fw/src/timer1.c
+++ fw/src/timer1.c
@@ -11,11 +11,11 @@ void timer1_init() {
   const uint16_t fudge = 62;
   // Setup a 2ms timer interrupt on TC1.
   TCCR1A  = 0;
-  TCCR1B  = (1 << CS10);                  // Use internal clock (16Mhz)
-  TCCR1B |= (1 << WGM12);                 // Clear timer on compare match
-  OCR1A   = (F_CPU / (500 + 1)) + fudge;  // 500Hz = 2ms
-  TIMSK1  = 1 << OCIE1A;                  // Enable compare match interrupt
-  TCNT1   = 0;                            // Reset the timer
+  TCCR1B  = (1 << CS10);                   // Use internal clock (16Mhz)
+  TCCR1B |= (1 << WGM12);                  // Clear timer on compare match
+  OCR1A   = (F_CPU / (1000 + 1)) + fudge;  // 1000Hz = 1ms
+  TIMSK1  = 1 << OCIE1A;                   // Enable compare match interrupt
+  TCNT1   = 0;                             // Reset the timer
 }
 
 ISR(TIMER1_COMPA_vect) {
