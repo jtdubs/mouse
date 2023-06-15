1e91114be335c1d95a98f0988f5e517b978d8290
Justin Dubs <jtdubs@gmail.com>
2023-06-15 18:50:12 -0400

    update TODO

--- fw/src/sim.c
+++ fw/src/sim.c
@@ -5,8 +5,6 @@ AVR_MCU(F_CPU, "atmega328p");
 AVR_MCU_VOLTAGES(5000, 5000, 5000);
 
 AVR_MCU_VCD_PORT_PIN('B', 0, "RIGHT_DIR");
-AVR_MCU_VCD_PORT_PIN('B', 1, "LEFT_PWM");
-AVR_MCU_VCD_PORT_PIN('B', 2, "RIGHT_PWM");
 AVR_MCU_VCD_PORT_PIN('B', 5, "LED_BUILTIN");
 AVR_MCU_VCD_PORT_PIN('C', 3, "PROBE_0");
 AVR_MCU_VCD_PORT_PIN('C', 4, "PROBE_1");
@@ -18,7 +16,7 @@ AVR_MCU_VCD_PORT_PIN('D', 5, "RIGHT_B");
 AVR_MCU_VCD_PORT_PIN('D', 6, "USER_IO");
 AVR_MCU_VCD_PORT_PIN('D', 7, "LEFT_DIR");
 
-AVR_MCU_VCD_IRQ(TIMER1_COMPA);
+AVR_MCU_VCD_IRQ(TIMER0_COMPA);
 AVR_MCU_VCD_IRQ(USART_UDRE);
 AVR_MCU_VCD_IRQ(USART_RX);
 
@@ -27,6 +25,9 @@ const struct avr_mmcu_vcd_trace_t _adc_trace[] _MMCU_ = {
     { AVR_MCU_VCD_SYMBOL("ADCSRA"), .what = (void*)&ADCSRA },
     { AVR_MCU_VCD_SYMBOL("ADMUX"),  .what = (void*)&ADMUX  },
     { AVR_MCU_VCD_SYMBOL("ADCL"),   .what = (void*)&ADCL   },
-    { AVR_MCU_VCD_SYMBOL("ADCH"),   .what = (void*)&ADCH   }
+    { AVR_MCU_VCD_SYMBOL("ADCH"),   .what = (void*)&ADCH   },
+    { AVR_MCU_VCD_SYMBOL("ICR1"),   .what = (void*)&ICR1   },
+    { AVR_MCU_VCD_SYMBOL("OCR1A"),  .what = (void*)&OCR1A  },
+    { AVR_MCU_VCD_SYMBOL("OCR1B"),  .what = (void*)&OCR1B  }
     // clang-format on}
 };
