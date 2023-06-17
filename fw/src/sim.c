abcf22ca3d54afd695956340c4b43d46e5f0aad0
Justin Dubs <jtdubs@gmail.com>
2023-06-16 22:55:55 -0400

    improve VCD signal names

--- fw/src/sim.c
+++ fw/src/sim.c
@@ -6,27 +6,37 @@
 AVR_MCU(F_CPU, "atmega328p");
 AVR_MCU_VOLTAGES(5000, 5000, 5000);
 
+// Right Motor
 AVR_MCU_VCD_PORT_PIN('B', 0, "RIGHT_DIR");
-AVR_MCU_VCD_PORT_PIN('B', 5, "LED_BUILTIN");
+AVR_MCU_VCD_PORT_PIN('D', 3, "RIGHT_CLK_A");
+AVR_MCU_VCD_PORT_PIN('D', 5, "RIGHT_B");
+
+// Left Motor
+AVR_MCU_VCD_PORT_PIN('D', 7, "LEFT_DIR");
+AVR_MCU_VCD_PORT_PIN('D', 2, "LEFT_CLK_A");
+AVR_MCU_VCD_PORT_PIN('D', 4, "LEFT_B");
+
+// Probes
 AVR_MCU_VCD_PORT_PIN('C', 3, "PROBE_0");
 AVR_MCU_VCD_PORT_PIN('C', 4, "PROBE_1");
 AVR_MCU_VCD_PORT_PIN('C', 5, "PROBE_2");
-AVR_MCU_VCD_PORT_PIN('D', 2, "LEFT_CLK_A");
-AVR_MCU_VCD_PORT_PIN('D', 3, "RIGHT_CLK_A");
-AVR_MCU_VCD_PORT_PIN('D', 4, "LEFT_B");
-AVR_MCU_VCD_PORT_PIN('D', 5, "RIGHT_B");
+
+// LEDs
+AVR_MCU_VCD_PORT_PIN('B', 5, "LED_BUILTIN");
 AVR_MCU_VCD_PORT_PIN('D', 6, "USER_IO");
-AVR_MCU_VCD_PORT_PIN('D', 7, "LEFT_DIR");
 
-AVR_MCU_VCD_IRQ(TIMER0_COMPA);
-AVR_MCU_VCD_IRQ(USART_UDRE);
-AVR_MCU_VCD_IRQ(USART_RX);
+// Interrupts
+AVR_MCU_VCD_IRQ_TRACE(INT0_vect_num, 1, "LEFT_ENCODER_CLK");
+AVR_MCU_VCD_IRQ_TRACE(INT1_vect_num, 1, "RIGHT_ENCODER_CLK");
+AVR_MCU_VCD_IRQ_TRACE(TIMER0_COMPA_vect_num, 1, "TICK");
+AVR_MCU_VCD_IRQ_TRACE(USART_UDRE_vect_num, 1, "SERIAL_TX");
+AVR_MCU_VCD_IRQ_TRACE(USART_RX_vect_num, 1, "SERIAL_RX");
 
-const struct avr_mmcu_vcd_trace_t _adc_trace[] _MMCU_ = {
+// PWM outputs
+const struct avr_mmcu_vcd_trace_t _pwm_trace[] _MMCU_ = {
     // clang-format off
-    { AVR_MCU_VCD_SYMBOL("ICR1"),   .what = (void*)&ICR1   },
-    { AVR_MCU_VCD_SYMBOL("OCR1A"),  .what = (void*)&OCR1A  },
-    { AVR_MCU_VCD_SYMBOL("OCR1B"),  .what = (void*)&OCR1B  }
+    { AVR_MCU_VCD_SYMBOL("LEFT_PWM"),  .what = (void*)&OCR1A  },
+    { AVR_MCU_VCD_SYMBOL("RIGHT_PWM"), .what = (void*)&OCR1B  }
     // clang-format on}
 };
 
