bf8a3ca2247a9544409230e71eee5032ee1b156c
Justin Dubs <jtdubs@gmail.com>
2023-06-26 18:01:36 -0400

    mouse drag-and-drop with reset

--- sim/pkg/ui/maze.go
+++ sim/pkg/ui/maze.go
@@ -1,7 +1,6 @@
 package ui
 
 import (
-	"fmt"
 	"math"
 	"strings"
 
@@ -29,6 +28,9 @@ func (s *mazeWindow) init() {
 }
 
 func (s *mazeWindow) draw() {
+	postDim := imgui.NewVec2(float32(len(s.sim.Maze.Posts[0])), float32(len(s.sim.Maze.Posts)))
+	gridDim := postDim.Sub(imgui.NewVec2(1, 1))
+
 	imgui.Begin("Maze")
 
 	imgui.Text("Maze:")
@@ -36,17 +38,21 @@ func (s *mazeWindow) draw() {
 	if imgui.ComboStr("##MAZE", &s.mazeIndex, s.mazeList) {
 		s.sim.Maze.Load(s.sim.Maze.ListMazes()[s.mazeIndex])
 	}
-	imgui.SameLine()
-	imgui.Text(fmt.Sprintf("(x=%02.2f, y=%02.2f, θ=%02.2f)", s.m.Environment.MouseX, s.m.Environment.MouseY, s.m.Environment.MouseAngle*180.0/math.Pi))
 
 	canvasSizePx := imgui.ContentRegionAvail()
-	postDim := imgui.NewVec2(float32(len(s.sim.Maze.Posts[0])), float32(len(s.sim.Maze.Posts)))
-	gridDim := postDim.Sub(imgui.NewVec2(1, 1))
 	cellSizePx := float32(math.Min(float64(canvasSizePx.X/gridDim.X), float64(canvasSizePx.Y/gridDim.Y)))
 	mazeSizePx := imgui.NewVec2(cellSizePx*gridDim.X, cellSizePx*gridDim.Y)
 	mmSizePx := cellSizePx / 180.0
 	marginPx := canvasSizePx.Sub(mazeSizePx).Div(2.0)
 	mazeOriginPx := imgui.CursorScreenPos().Add(imgui.NewVec2(0, canvasSizePx.Y)).Add(imgui.NewVec2(marginPx.X, -marginPx.Y))
+
+	imgui.Dummy(canvasSizePx)
+	if imgui.BeginPopupContextItem() {
+		if imgui.SelectableBool("Reset Position") {
+			s.m.Environment.MouseX, s.m.Environment.MouseY, s.m.Environment.MouseAngle = 90, 90, math.Pi/2
+		}
+		imgui.EndPopup()
+	}
 
 	drawList := imgui.WindowDrawList()
 	for i, row := range s.sim.Maze.Posts {
@@ -63,9 +69,6 @@ func (s *mazeWindow) draw() {
 
 	x, y, theta := float32(s.m.Environment.MouseX), float32(s.m.Environment.MouseY), s.m.Environment.MouseAngle
 	centerPx := mazeOriginPx.Add(imgui.NewVec2(x, -y).Mul(mmSizePx))
-
-	// 65mm in front of wheels, 35mm behind
-	// axle width is 90mm
 
 	mouseXY := func(mm imgui.Vec2) (px imgui.Vec2) {
 		deltaPx := mm.Mul(mmSizePx)
@@ -86,10 +89,56 @@ func (s *mazeWindow) draw() {
 		mouseXY(imgui.NewVec2(-35, 45)),
 	}
 
+	minX, minY, maxX, maxY := float32(math.Inf(1)), float32(math.Inf(1)), float32(math.Inf(-1)), float32(math.Inf(-1))
 	for i := range outline {
+		if outline[i].X < minX {
+			minX = outline[i].X
+		}
+		if outline[i].X > maxX {
+			maxX = outline[i].X
+		}
+		if outline[i].Y < minY {
+			minY = outline[i].Y
+		}
+		if outline[i].Y > maxY {
+			maxY = outline[i].Y
+		}
 		drawList.AddLineV(outline[i], outline[(i+1)%len(outline)], imgui.ColorConvertFloat4ToU32(imgui.NewVec4(0.5, 1, 0.5, 1)), 2.0)
 	}
 	drawList.AddLineV(mouseXY(imgui.NewVec2(0, 45)), mouseXY(imgui.NewVec2(0, -45)), imgui.ColorConvertFloat4ToU32(imgui.NewVec4(0.5, 1, 0.5, 1)), 2.0)
 
+	if dragging {
+		imgui.SetCursorScreenPos(imgui.NewVec2(dragButtonX, dragButtonY))
+	} else {
+		imgui.SetCursorScreenPos(imgui.NewVec2(minX, minY))
+	}
+	imgui.InvisibleButton("##mouse", imgui.NewVec2(maxX-minX, maxY-minY))
+	if imgui.IsItemHovered() {
+		wheel := float64(imgui.CurrentIO().MouseWheel())
+		if wheel != 0 {
+			angle := s.m.Environment.MouseAngle * 180.0 / math.Pi
+			angle = math.Round(angle/5.0) * 5.0
+			angle += 5.0 * wheel
+			s.m.Environment.MouseAngle = angle * math.Pi / 180.0
+		}
+	}
+	if imgui.IsItemActive() {
+		if !dragging {
+			dragStartX, dragStartY = s.m.Environment.MouseX, s.m.Environment.MouseY
+			dragButtonX, dragButtonY = minX, minY
+			dragging = true
+		} else {
+			delta := imgui.MouseDragDeltaV(imgui.MouseButtonLeft, 5.0).Mul(1.0 / mmSizePx)
+			s.m.Environment.MouseX = dragStartX + float64(delta.X)
+			s.m.Environment.MouseY = dragStartY - float64(delta.Y)
+		}
+	} else {
+		dragging = false
+	}
+
 	imgui.End()
 }
+
+var dragging = false
+var dragStartX, dragStartY float64
+var dragButtonX, dragButtonY float32
