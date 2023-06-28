29475d01d03499b322a3b0a556c8ca93e27d6f58
Justin Dubs <jtdubs@gmail.com>
2023-06-27 20:57:22 -0400

    back off to a 10ms tick timer

--- fw/src/platform/timer.c
+++ fw/src/platform/timer.c
@@ -11,12 +11,12 @@ static volatile bool timer_elapsed;
 
 // timer_init initializes timer.
 void timer_init() {
-  // Setup a 1ms timer interrupt on Timer0.
-  TCCR0A = (1 << WGM01);               // Clear timer on compare match
-  TCCR0B = (1 << CS00) | (1 << CS01);  // Use clk/64 prescaler (250kHz)
-  OCR0A  = (F_CPU / 64 / (1000 + 1));  // 1000Hz = 1ms
-  TIMSK0 = 1 << OCIE0A;                // Enable compare match interrupt
-  TCNT0  = 0;                          // Reset the timer
+  // Setup a 10ms timer interrupt on Timer0.
+  TCCR0A = (1 << WGM01);                    // Clear timer on compare match
+  TCCR0B = (1 << CS00) | (1 << CS02);       // Use clk/1024 prescaler (15.625kHz)
+  OCR0A  = (F_CPU / 1024 / (100 + 1)) + 1;  // 100Hz = 10ms
+  TIMSK0 = 1 << OCIE0A;                     // Enable compare match interrupt
+  TCNT0  = 0;                               // Reset the timer
 
   // Enable watchdog timer to reset the device if the timer interrupt fails.
   wdt_enable(WDTO_15MS);
