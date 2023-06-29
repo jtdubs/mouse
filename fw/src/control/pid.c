bb97c8de7dae36a8e33cbbd621f13d789e380a5d
Justin Dubs <jtdubs@gmail.com>
2023-06-28 23:25:05 -0400

    stuff

--- fw/src/control/pid.c
+++ fw/src/control/pid.c
@@ -3,24 +3,23 @@
 #include <math.h>
 
 float pid_update(pid_t *pid, float sp /* setpoint */, float pv /* process variable */) {
-  float last_e = pid->e;
   pid->e       = sp - pv;
-  float d      = last_e - pid->e;
-  if (pid->e == 0 && d == 0) {
-    pid->i = 0;
-    return 0;
-  }
-  float i   = pid->i + pid->e;
-  float out = fmaf(pid->kp, pid->e, fmaf(pid->ki, i, pid->kd * d));
+  float d      = pid->last_pv - pv;  // maybe backwards??
+  pid->last_pv = pv;
+
+  float i   = pid->i + (pid->ki * pid->e);  // + (pid->kp * e); ??
+  float out = fmaf(pid->kp, pid->e, fmaf(pid->kd, d, i));
   if (out >= pid->min && out < pid->max) {
     // Only update the integrator if the output is within the min/max range.
     // This helps prevent the integrator from running away.
     pid->i = i;
   }
+
   return fmaxf(pid->min, fminf(pid->max, out));
 }
 
 void pid_reset(pid_t *pid) {
-  pid->e = 0;
-  pid->i = 0;
+  pid->e       = 0;
+  pid->i       = 0;
+  pid->last_pv = 0;
 }
--- fw/src/control/pid.h
+++ fw/src/control/pid.h
@@ -6,6 +6,7 @@ typedef struct {
   float min, max;    // bounds for pid output
   float kp, ki, kd;  // pid constants
   float e, i;        // error and integral
+  float last_pv;
 } pid_t;
 
 float pid_update(pid_t *pid, float sp, float pv);
