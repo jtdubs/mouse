f1f6216262843384c65c8203cfb1ed3df75379f6
Justin Dubs <jtdubs@gmail.com>
2023-07-02 22:22:11 -0400

    fix wheel bias to match board

--- sim/pkg/sim/mouse.go
+++ sim/pkg/sim/mouse.go
@@ -118,9 +118,9 @@ func (m *Mouse) SetPosition(x, y, angle float64) {
 func (m *Mouse) EncoderTick(left, forward bool) {
 	var fudge float64
 	if left {
-		fudge = 1.0 + 0.01
+		fudge = 1.0 + 0.002
 	} else {
-		fudge = 1.0 - 0.01
+		fudge = 1.0 - 0.002
 	}
 	m.SetPosition(
 		m.X+(m.dx(left, forward)*fudge),
