7efec68c6c8d18dfb9e396c65abf23ce4ec32a35
Justin Dubs <jtdubs@gmail.com>
2023-07-02 21:01:46 -0400

    redo all the braking logic

--- fw/src/control/linear.c
+++ fw/src/control/linear.c
@@ -11,7 +11,6 @@
 float linear_start_distance;   // mm
 float linear_target_distance;  // mm
 float linear_target_speed;     // rpm
-bool  linear_braking;
 
 static pi_t  linear_pi_theta;
 static float linear_theta_setpoint;
@@ -30,7 +29,6 @@ void linear_start(float distance /* mm */, bool stop) {
   linear_start_distance  = position_distance;                        // mm
   linear_target_distance = position_distance + distance;             // mm
   linear_target_speed    = stop ? 0.0 : SPEED_TO_RPM(SPEED_CRUISE);  // RPMs
-  linear_braking         = false;
   linear_theta_setpoint  = position_theta;
   pi_reset(&linear_pi_theta);
 }
@@ -42,31 +40,27 @@ bool linear_update() {
     return true;
   }
 
-  float current_speed = (speed_setpoint_left + speed_setpoint_right) / 2.0;  // RPMs
+  float current_speed    = (speed_measured_left + speed_measured_right) / 2.0;  // RPMs
+  float current_setpoint = (speed_setpoint_left + speed_setpoint_right) / 2.0;  // RPMs
 
   // Compute the braking distance.
-  float braking_point = +INFINITY;  // mm
-  if (current_speed > CLAMP_RPM(linear_target_speed)) {
-    float brake_time = TICKS_TO_S((current_speed - CLAMP_RPM(linear_target_speed)) / ACCEL_TO_RPM(ACCEL_DEFAULT));  // s
-    float brake_distance  = RPM_TO_SPEED(current_speed) * brake_time;       // mm
-    brake_distance       -= 0.5 * ACCEL_DEFAULT * brake_time * brake_time;  // mm
-    braking_point         = linear_target_distance - brake_distance;        // mm
+  float braking_rate = 0;  // RPMs
+  if (current_speed > linear_target_speed) {
+    float deltaV = RPM_TO_SPEED(linear_target_speed - current_speed);  // mm/s
+    float deltaX = linear_target_distance - position_distance;         // mm
+    braking_rate =
+        -ACCEL_TO_RPM(((2.0 * RPM_TO_SPEED(current_speed) * deltaV) + (deltaV * deltaV)) / (2.0 * deltaX));  // RPMs
   }
 
   float new_speed;
 
-  // If we are in the braking zone, decelerate.
-  if (position_distance >= braking_point) {
-    new_speed      = current_speed - ACCEL_TO_RPM(ACCEL_DEFAULT);  // RPMs
-    linear_braking = true;
+  // If we need to brake, do so.
+  if (braking_rate > ACCEL_TO_RPM(ACCEL_DEFAULT)) {
+    new_speed = current_setpoint - braking_rate;  // RPMs
   }
-  // If we are not in the braking zone, and are under cruise speed, accelerate.
-  else if (!linear_braking && current_speed < SPEED_TO_RPM(SPEED_CRUISE)) {
-    new_speed = CLAMP_RPM(current_speed + ACCEL_TO_RPM(ACCEL_DEFAULT));  // RPMs
-  }
-  // If we were in the braking zone, that means we've slowed too quickly, so just hold speed.
-  else if (linear_braking) {
-    new_speed = current_speed;
+  // Otherwise, if we are under cruise speed, accelerate.
+  else if (current_setpoint < SPEED_TO_RPM(SPEED_CRUISE)) {
+    new_speed = current_setpoint + ACCEL_TO_RPM(ACCEL_DEFAULT);  // RPMs
   }
   // Otherwise, continue at cruise speed.
   else {
@@ -74,10 +68,14 @@ bool linear_update() {
   }
 
   // Adjust steering to maintain angle.
+  // This should probably just multiply the final speed by 1+-BIAS rather than doing this PI stuff...
   linear_theta_measured = (linear_theta_alpha * position_theta) + ((1.0 - linear_theta_alpha) * linear_theta_measured);
   float steering_adjustment = pi_update(&linear_pi_theta, linear_theta_setpoint, linear_theta_measured);
 
-  speed_set(new_speed - steering_adjustment, new_speed + steering_adjustment);
+  float left_speed  = CLAMP_RPM(new_speed - steering_adjustment);
+  float right_speed = CLAMP_RPM(new_speed + steering_adjustment);
+
+  speed_set(left_speed, right_speed);
   speed_update();
   return false;
 }
